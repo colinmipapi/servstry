1f686ab909f9c020b042be5ceb2a7d2701caea52
Colin J McFaul <colinjmcfaul@gmail.com>
2020-05-22 15:22:16 -0400

    commit before pushing to heroku

--- contact_trace/settings.py
+++ contact_trace/settings.py
@@ -17,13 +17,16 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env = environ.Env(
-    DEBUG=(bool, False),
-    LOCAL=(bool, False)
-)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+try:
+    env = environ.Env(
+        DEBUG=(bool, False),
+        LOCAL=(bool, False)
+    )
+    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-LOCAL = env('LOCAL')
+    LOCAL = env('LOCAL')
+except:
+    LOCAL = False
 
 if LOCAL:
     SECRET_KEY = env('SECRET_KEY')
