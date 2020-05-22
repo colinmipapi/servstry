6bd679081dcb4d70285c7fb48de5fcaebe2a8fd4
Colin J McFaul <colinjmcfaul@gmail.com>
2020-05-22 16:03:05 -0400

    commit before pushing to heroku

--- contact_trace/settings.py
+++ contact_trace/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import environ
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if not os.environ.get('DEBUG'):
+    import environ
     env = environ.Env(
         DEBUG=(bool, False),
     )
