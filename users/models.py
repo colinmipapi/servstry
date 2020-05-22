455ba676823dba2d2ef2d0b4e0427dc1257cf098
Colin J McFaul <colinjmcfaul@gmail.com>
2020-05-22 14:00:47 -0400

    first commit

---
+++ users/models.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    public_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+    )
